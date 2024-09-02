6d98b2411da19d8580b53efa74f5e604e305a8dd
Eesha Pranav <120297278+eeshapranav@users.noreply.github.com>
2024-09-02 20:05:26 +0530

    Create 2. Search Insert Position.java

---
+++ 2. Search Insert Position.java
@@ -0,0 +1,24 @@
+35. Search Insert Position
+
+Time Complexity: O(n).
+Space Complexity: O(1).
+
+  class Solution 
+{
+    public int searchInsert(int[] nums, int target) 
+    {
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+            else if(target<nums[i])
+            {
+                return i;
+            }
+        }
+
+    return nums.length;
+    }
+}
