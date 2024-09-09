5ce95493e4b2492d51540c9c89190ddf8454e1a0
Eesha Pranav <120297278+eeshapranav@users.noreply.github.com>
2024-09-09 22:10:15 +0530

    Create 38. Merge Sorted Array.java

---
+++ 38. Merge Sorted Array.java
@@ -0,0 +1,18 @@
+88. Merge Sorted Array
+
+Time Complexity: O((m+n)log(m+n)).
+Space Complexity: O(1).
+
+class Solution 
+{
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        for (int j = 0, i = m; j < n; j++) 
+        {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+
