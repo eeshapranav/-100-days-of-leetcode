261da7469b0bd0b65ae4596dab7e4be26e13ebb3
Eesha Pranav <120297278+eeshapranav@users.noreply.github.com>
2024-09-18 22:26:19 +0530

    Create 53. Reverse Linked List.java

---
+++ 53. Reverse Linked List.java
@@ -0,0 +1,22 @@
+206. Reverse Linked List
+
+Time Complexity : O(n).
+Space Complexity : O(1).
+
+class Solution 
+{
+    public ListNode reverseList(ListNode head) 
+    {
+        ListNode prev = null;  
+        ListNode current = head;
+    
+        while(current != null) 
+        { 
+            ListNode next = current.next; 
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+       return prev; 
+    }
+}
